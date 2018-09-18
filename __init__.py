e01f627168370506abce196fe93ca650e23b2fc1
Ryan Bradley <bradleyrp@gmail.com>
2018-09-18 14:05:45 -0400

    ortho branch for new automacs

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from geometry_tools.mesh import *
+from geometry_tools.structural_biology import *
+from geometry_tools.Multimer import *
+from multiply import *
+
+_shared_extensions = ['rotation_matrix']
+_not_reported = ['triarea','vecnorm','facenorm','torusnorm','reclock','beyonder','makemesh','rotation_matrix']
---
+++ geometry_tools/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from mesh import *
+from structural_biology import *
+from geometry_tools import  *
+from Multimer import *
+#! trying to use magic_importer from amx here
+from amx import state
+mesh.state = state
+structural_biology.state = state
+#! etc
+
+#from ortho.imports import magic_importer
+#import os
+#magic_importer(globals(),os.path.dirname(__file__))
