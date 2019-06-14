72976e05881940985efe4b0b4006bc7d7da8b5ae
Ryan Bradley <bradleyrp@gmail.com>
2019-06-14 12:40:20 -0400

    updated imports for python 3

--- geometry_tools/__init__.py
+++ geometry_tools/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from mesh import *
-from structural_biology import *
-from geometry_tools import  *
-from Multimer import *
+from .mesh import *
+from .structural_biology import *
+from .geometry_tools import  *
+from .Multimer import *
 #! trying to use magic_importer from amx here
 from amx import state
 mesh.state = state
