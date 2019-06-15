c21f26f85ec6fa89f24d583dbbe164946ba01b27
Ryan Bradley <bradleyrp@gmail.com>
2019-06-14 22:04:51 -0400

    added a hard restart feature

--- scripts/restart.py
+++ scripts/restart.py
@@ -5,21 +5,25 @@ RESTART a GROMACS simulation from minimal components.
 """
 
 from amx import *
+import glob,re,shutil,os
+
+def unique_input(suffix,where='inputs'):
+	"""Check for a unique file by suffix."""
+	has_suffix = r'^(.*)\.%s$'
+	fns = glob.glob(os.path.join(where,'*'))
+	fns = [os.path.realpath(i) for i in fns 
+		if re.match(has_suffix%suffix,os.path.basename(i))]
+	if len(fns)==1: return fns[0]
+	else: return None
 
 make_step(settings.step)
 
-import glob,re,shutil
 # detect a single cpt and tpr in inputs
 if settings.detect=='inputs':
-	has_suffix = r'^(.*)\.%s$'
-	fns = glob.glob('inputs/*')
-	fns_tpr,fns_cpt = [[os.path.realpath(i) for i in fns 
-		if re.match(has_suffix%suf,os.path.basename(i))]
-		for suf in ['tpr','cpt']]
-	if len(fns_tpr)!=1 or len(fns_cpt)!=1:
-		raise Exception('cannot find unique cpt/tpr: %s, %s'%(
-			list(fns_cpt),list(fns_tpr)))
-	cpt,tpr = fns_cpt[0],fns_tpr[0]	
+	cpt = unique_input('cpt',where=settings.detect)
+	tpr = unique_input('tpr',where=settings.detect)
+	if not cpt or not tpr:
+		raise Exception('cannot find a CPT and TPR file in %s'%settings.detect)
 else: raise Exception('unclear settings.detect: %s'%settings.detect)
 
 ans = gmx('dump',cp=cpt)
@@ -42,50 +46,69 @@ elif settings.extend:
 	when = settings.extend
 else: raise Exception('dev')
 
-"""
-note that GROMACS throws an error if the cpt file step number is too late"
-	The input requested 20000 steps, however the checkpoint file has already
-	reached step 19498950. The simulation will not proceed, because either your
-	simulation is already complete, or your combination of input files don't
-	match.
-to manage this, you should either start the simulation from a new structure
-and MDP file (ideally, using a soon-to-be developed)
-"""
-
+#! sidestep the error in which the cpt step is too far
 if time_start!=0 and instruct_extend=='extend':
 	instruct_extend = 'until'
 	when = when + time_start
 
-# convert the TPR
-convert_kwargs = {instruct_extend:when}
-checkpoint_in = 'start.cpt'
-gmx('convert-tpr',input=tpr,
-	log='continue-tpr',out='start.tpr',**convert_kwargs)
-shutil.copyfile(cpt,state.here+checkpoint_in)
+# standard restart requires a cpt/tpr only
+mdrun_kwargs = {}
+if not settings.hard:
+	# convert the TPR
+	convert_kwargs = {instruct_extend:when}
+	checkpoint_in = 'start.cpt'
+	gmx('convert-tpr',input=tpr,
+		log='continue-tpr',out='start.tpr',**convert_kwargs)
+	shutil.copyfile(cpt,state.here+checkpoint_in)
+	mdrun_kwargs['checkpoint_in'] = checkpoint_in
+# hard restart from a derived structure with new mdp options
+else:
+	# detects: mdp, top, cpt, tpr and optional ndx and ff folders
+	#! dev: consider a json file to annotate the inputs
+	mdp = unique_input('mdp',where=settings.detect)
+	top = unique_input('top',where=settings.detect)
+	if not mdp or not top: 
+		raise exception('we require an mdp and top at %s'%settings.detect)
+	ndx = unique_input('ndx',where=settings.detect)
+	# collect force field folders
+	ffs = [os.path.realpath(fn) 
+		for fn in glob.glob(os.path.join(settings.detect,'*'))
+		if re.match(r'^.+\.ff$',fn)
+		and os.path.isdir(fn)]
+	links = ffs+[mdp,top]
+	if ndx: links += [ndx]
+	for fn in links: 
+		os.symlink(fn,os.path.join(state.here,os.path.basename(fn)))
+	# built a structure from the checkpoint
+	structure = 'start.gro'
+	kwargs = dict(inpipe='0\n')
+	gmx('trjconv',structure=cpt,input=tpr,
+		out=structure,log='trjconv-cpt-structure',**kwargs)
+	kwargs = dict(out='start.tpr',structure=structure,parameters=mdp,
+		parameters_out='start.mdp',topology=top)
+	if ndx: kwargs['groups'] = ndx
+	gmx('grompp',log='grompp-restart',**kwargs)
 
 # run mdrun
 if not settings.legacy_part_names:
 	"""
-	the new default part names are:
-		traj.part0002.trr
-		traj_comp.part0002.xtc
-		ener.part0002.edr
-		md.part0002.log
+	the new default part names are: traj.part0002.trr, traj_comp.part0002.xtc,
+	ener.part0002.edr, md.part0002.log, etc.
 	and gromacs will autodetect as long as you use the call below
 	"""
 	gmx('mdrun',
 		input='start.tpr',
-		checkpoint_in=checkpoint_in,
-		noappend=True,
-		log='mdrun-part0002')
+		log='mdrun-part0002',
+		**mdrun_kwargs)
 # legacy naming scheme for a restart
 else:
 	base = 'md.part0002'
 	gmx('mdrun',noappend=True,
-		checkpoint_in=checkpoint_in,
+
 		compressed='%s.xtc'%name,
 		energy='%s.edr'%name,
 		logfile='%s.log'%name,
 		structure='%s.gro'%name,
 		trajectory='%s.trr'%name,
-		log='mdrun-part0002')
+		log='mdrun-part0002',
+		**mdrun_kwargs)
